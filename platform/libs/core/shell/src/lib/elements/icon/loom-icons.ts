b92bd7cb61d48cd183bb8aaeb256050ded92bedd
Norbert Rosenwinkel <norbert@rosenwinkel.com>
2026-09-06 18:35:25 +0200

    feat(shell): the fold control shows three dots, a new `more` icon
    Signed-off-by: Norbert Rosenwinkel <norbert@rosenwinkel.com>

--- platform/libs/core/shell/src/lib/elements/icon/loom-icons.ts
+++ platform/libs/core/shell/src/lib/elements/icon/loom-icons.ts
@@ -14,6 +14,7 @@ import {
   heroCog6Tooth,
   heroComputerDesktop,
   heroDocument,
+  heroEllipsisHorizontal,
   heroEye,
   heroExclamationTriangle,
   heroInformationCircle,
@@ -68,6 +69,7 @@ export const LOOM_ICONS = {
   workspaces: heroRectangleGroup,
 
   menu: heroBars3,
+  more: heroEllipsisHorizontal,
   document: heroDocument,
   openWork: heroViewColumns,
   navigator: heroBars3,
