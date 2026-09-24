f2efffb3ccd10a14c41fe71c46384d913e6fa30b
Norbert Rosenwinkel <norbert@rosenwinkel.com>
2026-09-24 17:24:18 +0200

    fix(demo): quote commands accept quotes created after startup
    Signed-off-by: Norbert Rosenwinkel <norbert@rosenwinkel.com>

---
+++ demo/src/quotes/src/lib/plugin/quotes-commands.spec.ts
@@ -0,0 +1,38 @@
+import type {
+  ChoiceCommandArgument,
+  Command,
+  PluginContext,
+} from '@loomweaver/plugin-sdk';
+import { addQuote, resetQuotes } from '../../../../accounting';
+import { registerQuoteCommands } from './quotes-commands';
+
+function registeredCommands(): Command[] {
+  const commands: Command[] = [];
+  const ctx = {
+    registerCommand: (command: Command) => {
+      commands.push(command);
+      return { dispose: () => undefined };
+    },
+  } as unknown as PluginContext;
+  registerQuoteCommands(ctx);
+  return commands;
+}
+
+function numberChoices(commands: readonly Command[], id: string): readonly string[] {
+  const command = commands.find((candidate) => candidate.id === id);
+  return (command?.arguments?.[0] as ChoiceCommandArgument).choices;
+}
+
+describe('the quote commands', () => {
+  afterEach(() => resetQuotes());
+
+  it('accept a quote created after they were registered', () => {
+    const commands = registeredCommands();
+
+    const created = addQuote('c-nordwind');
+
+    for (const id of ['quotes.open', 'quotes.send', 'quotes.margin']) {
+      expect(numberChoices(commands, id)).toContain(created.number);
+    }
+  });
+});
